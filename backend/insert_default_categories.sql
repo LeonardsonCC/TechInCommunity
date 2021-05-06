233448e784742ca0ad8eea5bea3ba145123def21
Thiago N <thiago@desenvolvedor.tech>
2021-05-05 22:06:02 -0300

    Adicionado arquivo com insert das categorias padrão

---
+++ backend/insert_default_categories.sql
@@ -0,0 +1,5 @@
+USE mercatop;
+
+INSERT INTO category(name) VALUES ('Carnes'),('Alimentos básicos'),('Bebidas'),('Bebidas alcolicas'),('Congelados'),('Feira'),('Frios e laticínios'),('Leites e iogurtes'),('Matinais'),('Padaria'),('Biscoitos e salgadinhos'),('Doces e sobremesas'),('Limpeza'),('Higiene'),('Cuidados de pele');
+
+SELECT * FROM category;
