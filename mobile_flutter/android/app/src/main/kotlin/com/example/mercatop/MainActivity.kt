6e19b1e740e8363f60fd599e85389af6cdf8441c
Thiago N <thiago@desenvolvedor.tech>
2021-06-12 23:06:07 -0300

    Lista de categorias adicionada no Flutter

---
+++ mobile_flutter/android/app/src/main/kotlin/com/example/mercatop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mercatop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile_flutter/lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart' show rootBundle;
+
+void main() {
+  runApp(new MaterialApp(
+    home: new CategoryPage()
+  ));
+}
+
+class CategoryPage extends StatefulWidget {
+  @override
+  CategoryPageState createState() => new CategoryPageState();
+}
+
+class CategoryPageState extends State<CategoryPage> {
+
+ 	List data;
+ 	Map config;
+
+	Future<String> getConfig() async {
+		this.config = json.decode(await rootBundle.loadString('config.json'));
+		return "Success!";
+	}
+
+	Future<String> getData() async {
+		this.config = json.decode(await rootBundle.loadString('config.json'));
+
+		var response = await http.get(
+			Uri.encodeFull(this.config["url"]+"/category?supermarket_id="+this.config["supermarket_id"]),
+			headers: {
+		    	"Accept": "application/json"
+		  	}
+		);
+
+		this.setState(() {
+		  	data = json.decode(response.body);
+		});
+
+		return "Success!";
+	}
+
+	@override
+	void initState(){
+		this.getConfig();
+		this.getData();
+	}
+
+	@override
+	Widget build(BuildContext context){
+		return new Scaffold(
+		  appBar: new AppBar(title: new Text("Categorias"), backgroundColor: Colors.blue),
+		  body: new ListView.builder(
+		    itemCount: data == null ? 0 : data.length,
+		    itemBuilder: (BuildContext context, int index){
+		      return new Card(
+				child: Row(
+				     children: <Widget>[
+				         Image.network(this.config["url"]+this.config["image_route"]+data[index]["picture"], height: 150, width: 150),
+				         Text(data[index]["name"]),
+				     ],
+				)
+		      );
+		    },
+		  ),
+		);
+	}
+}
