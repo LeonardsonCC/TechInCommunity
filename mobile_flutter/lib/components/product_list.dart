ebd75c64318e2dcf3f741053c554785c50bc532d
Thiago N <thiago@desenvolvedor.tech>
2021-06-13 11:50:45 -0300

    Lista de produtos adicionada (Flutter)

--- mobile_flutter/lib/components/product_list.dart
+++ mobile_flutter/lib/components/product_list.dart
@@ -4,28 +4,27 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:mercatop/components/product_list.dart';
 
-class CategoryPage extends StatefulWidget {
+class ProductPage extends StatefulWidget {
   @override
-  CategoryPageState createState() => new CategoryPageState();
+  ProductPageState createState() => new ProductPageState();
 }
 
-class CategoryPageState extends State<CategoryPage> {
+class ProductPageState extends State<ProductPage> {
 
  	List data;
  	Map config;
+ 	var args;
 
 	Future<String> getConfig() async {
 		this.config = json.decode(await rootBundle.loadString('config.json'));
 		return "Success!";
 	}
 
-	Future<String> getData() async {
+	Future<String> getData(args) async {
 		this.config = json.decode(await rootBundle.loadString('config.json'));
-
 		var response = await http.get(
-			Uri.encodeFull(this.config["url"]+"/category?supermarket_id="+this.config["supermarket_id"]),
+			Uri.encodeFull(this.config["url"]+"/product/search?supermarket_id="+this.config["supermarket_id"]+"&category_id="+args["category_id"].toString()),
 			headers: {
 		    	"Accept": "application/json"
 		  	}
@@ -40,33 +39,39 @@ class CategoryPageState extends State<CategoryPage> {
 
 	@override
 	void initState(){
+	    Future.delayed(Duration.zero, () {
+	      setState(() {
+	        args = ModalRoute.of(context).settings.arguments;
+	      });
+	      this.getData(args);
+	    });
+
 		this.getConfig();
-		this.getData();
 	}
 
 	@override
 	Widget build(BuildContext context){
 		return new Scaffold(
-		  appBar: new AppBar(title: new Text("Categorias"), backgroundColor: Colors.blue),
+		  appBar: new AppBar(title: new Text("Produtos"), backgroundColor: Colors.blue),
 		  body: new ListView.builder(
 		    itemCount: data == null ? 0 : data.length,
 		    itemBuilder: (BuildContext context, int index){
 			return new	Card(
 				  child: new InkWell(
 				    onTap: () {
-						  Navigator.push(
-						    context,
-						    MaterialPageRoute(
-						    	builder: (context) => ProductPage(),
-						        settings: RouteSettings(arguments: {'category_id': data[index]["id"]},
-                  ),),
-						  );
+						  Navigator.pop(context);
 				    },
 				    child: Row(
 				     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 				     children: <Widget>[
 				         Image.network(this.config["url"]+this.config["image_route"]+data[index]["picture"], height: 150, width: 150),
-				         Text(data[index]["name"], style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+						 Column(
+					          mainAxisAlignment: MainAxisAlignment.center,
+					          children: [
+					            Text(data[index]["name"], style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+					            Text("R\$"+data[index]["price"].toString()+"/"+data[index]["unit"]),
+					          ]
+						),
 				     ],
 					),
 				  ),
