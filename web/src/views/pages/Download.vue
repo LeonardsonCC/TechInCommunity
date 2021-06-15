41bdafa8e0fa227a6e51ee0115c64bdb617d76e4
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-06-15 05:59:04 -0300

    Feedback de que o servidor está processando o app

--- web/src/views/pages/Download.vue
+++ web/src/views/pages/Download.vue
@@ -5,29 +5,57 @@
           <BaseCard heading="Funções">
             <v-btn
               color="primary"
-              dark
               @click="downloadApk"
+              :disabled="isDownloading"
+              :loading="isDownloading"
             >
               Download
             </v-btn>
           </BaseCard>
         </v-col>
     </v-row>
+    <v-snackbar
+      v-model="snackbar.show"
+      :color="snackbar.color"
+    >
+      {{ snackbar.message }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="snackbar.show = false"
+        >
+          Fechar
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-container>
 </template>
 
 <script>
 import { download } from "../../providers/api/supermarket";
-/*import FileDownload from "js-file-download";*/
 
 export default {
   name: "Download",
 
   data: () => ({
+    isDownloading: false,
+    snackbar: {
+      show: false,
+      message: "",
+      color: "white",
+    },
   }),
 
   methods: {
       downloadApk: function () {
+          const self = this;
+          self.isDownloading = true;
+          self.snackbar.show = true;
+          self.snackbar.message = "Estamos gerando uma nova versão do app para você!";
+          self.snackbar.color = "blue";
           download()
             .then((response) => {
                 console.log(response)
@@ -35,8 +63,18 @@ export default {
                 let url = window.URL.createObjectURL(blob)
 
                 window.open(url)
+                self.isDownloading = false;
+                self.snackbar.show = true;
+                self.snackbar.message = "Novo aplicativo gerado! Fazendo download...";
+                self.snackbar.color = "green";
               })
-            .catch((err) => console.log(err))
+            .catch((err) => {
+                console.log(err)
+                self.isDownloading = false;
+                self.snackbar.show = true;
+                self.snackbar.message = "Ocorreu algum erro ao gerar o aplicativo... Por favor, tente novamente";
+                self.snackbar.color = "red";
+              })
       }
   }
 };
